69e5ffce23c437030d683a5ba4ca58388e8da5a1
Saevitus <VirAeternus@valveanticheat.com>
2017-04-17 14:25:52 +1000

    Update Utilities.h

--- SteamworksMeme/Header Files/Utilities.h
+++ SteamworksMeme/Header Files/Utilities.h
@@ -1,16 +1,19 @@
 #pragma once
 #include <conio.h> // needed for _getch();
 #include <cstdio> // needed for printf();
+#include <string> // needed for std::string
 #include "steam_api.h"
+
+#define MISC_FUNCS
 
 // EFriendFlags
 #define ALL_FRIENDS 0xFFFF
 
 namespace Utilities
 {
-	// idea from syn, just a basic wrapper for printf();
+	// copypasta from syn, just a basic wrapper for printf();
 	// using va_list
-	void Log(const char* fmt, ...);
+	void Log(std::string strFmt, ...);
 
 	// basically does the same as system("pause");
 	// just gets input inside the console window, and will exit the program.
@@ -28,11 +31,11 @@ namespace Memeworks
 	// inline because it'd be a waste of my time to do it in a source file
 	inline bool RequestUserStats() { return  SteamUserStats()->RequestCurrentStats(); }
 
-	// prints the SteamID3 of the account currently logged in
+	// prints the SteamID3
 	void GetSteamID3();
 
 	// well... if you don't understand xd
-	void SetName(const char* pchName);
+	void SetName(std::string strName);
 
 	// prints your current steam level
 	void GetSteamLevel();
@@ -45,15 +48,34 @@ namespace Memeworks
 
 	// sets the rich presence of your status on steam, and ingame score
 	// for games like csgo, to any string you input.
-	void SetPoorPresence(const char* pchStatus, const char* pchScore);
+	void SetPoorPresence(std::string strStatus, std::string strScore);
+
+	// each achievement for games on steam have an internal name
+	// which could be like FUCKING_KILL_ME or suicideiscool3
+	// doesn't matter, either way this fucking gets them
+	void GetInternalAchievementID(int iAmount);
+
+	// to be used in conjunction with GetInternalAchievementID
+	// use the internal name in this, if you want to unlock a single
+	// achievement, only for current appid
+	void UnlockSingleAchievement(std::string strName);
 
 	// unlocks achievements up to the number you input
 	// only for current appid
-	// TODO: add custom achievement unlocking
 	void UnlockAchievements(int iAmount);
 
 	// clears achievements up to the number you input
 	// only for current appid
 	// TODO: add custom achievement clearing
 	void ClearAchievements(int iAmount);
+
+	// testing namespace so I can stop filling
+	// main() and memeworks with useless testing shit
+	namespace Testing
+	{
+		void FindLobby(int iFriendIndex);
+		
+		void MiscFunc1();
+
+	}
 }
