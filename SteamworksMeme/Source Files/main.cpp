c27764b28d6151987f99d927d28730fdea763a2e
Saevitus <VirAeternus@valveanticheat.com>
2017-04-17 14:27:53 +1000

    yeah
    xD

--- SteamworksMeme/Source Files/main.cpp
+++ SteamworksMeme/Source Files/main.cpp
@@ -1,20 +1,32 @@
 #include "../Header Files/Utilities.h"
+#include "../Header Files/Memory.h"
 
 void main()
 {
+	// simple steam api shit, tells us the path to steam.exe and
+	// if steam is running or not
 	Utilities::Log("Steam Location: %s", SteamAPI_GetSteamInstallPath());
 	Utilities::Log("Steam Running: %s", SteamAPI_IsSteamRunning() ? "true" : "false");
 
+	// all this other shit has been documented in the header files you fuckwit
 	if (Memeworks::Initiated())
 	{
 		Utilities::Log("Requsted User Statistics: %s", Memeworks::RequestUserStats() ? "true" : "false");
 		Memeworks::GetSteamID3();
-		Memeworks::SetName("meme");
 		Memeworks::GetSteamLevel();
 		Memeworks::GetFriendCount(ALL_FRIENDS);
-		Memeworks::UnlockAchievements(127); // 167(678)
-		//Memeworks::ClearAchievements(167);
-		Memeworks::SetPoorPresence("hyper big gay", "payhack.net ~ payhack.net ~ payhack.net ~ payhack.net ~ payhack.net");
+		Memeworks::Testing::MiscFunc1();
+		Memeworks::UnlockAchievements(520); // tf2 has 520 achievements
+
+		/*Memory::Process proc = Memory::Process("csgo.exe");
+		Memory::Module mod = proc.FindModule("client.dll");
+
+		Utilities::Log("PID: %i", proc.GetPID());
+
+		DWORD base = mod.GetBase();
+		DWORD localplayer = proc.ReadMemory<DWORD>(base + 0xAA66D4);*/
+
+		//Memeworks::GetInternalAchievementID(12);
 
 		Utilities::WaitForInput();
 		SteamAPI_Shutdown();
